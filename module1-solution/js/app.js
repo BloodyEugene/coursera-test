6f1aedcc60700ff2a0dacf3edddfa369a051406b
Evgeny Kolbasov <eyk7bf@gmail.com>
2019-11-04 22:58:00 +0600

    the task is done

---
+++ module1-solution/js/app.js
@@ -0,0 +1,30 @@
+(function() {
+  'use strict';
+
+  angular.module('LunchCheck', [])
+
+  .controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController($scope) {
+    $scope.message = '';
+    $scope.foodList = '';
+
+    $scope.check = function () {
+      if ($scope.foodList.length === 0) {
+        $scope.message = 'Please enter data first';
+        return;
+      }
+
+      var actualNumber = 0;
+      for (var value of $scope.foodList.split(",")) {
+        if (value.length > 0) {
+          actualNumber += 1;
+        }
+      }
+
+      $scope.message = actualNumber <= 3 ? 'Enjoy!' : 'Too much!';
+    };
+  }
+
+})();
